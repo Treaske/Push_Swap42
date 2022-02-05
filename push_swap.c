37d3265b6b7dbf4f0f1d02aa39a6f97281894e5d
Treaske <78087802+Treaske@users.noreply.github.com>
2022-02-05 20:03:02 +0100

    Trying arguments

--- push_swap.c
+++ push_swap.c
@@ -39,23 +39,133 @@ int	ft_atoi(const char *str, t_strc_gen *est)
 	}
 	return (num * sign);
 }
-
-int *ft_get_number(int len, char **argv, t_strc_gen *est)
+ // split-----------------------------------------------
+int ft_count_arg(char *argv)
 {
+	int num;
+	int x;
+
+	num = 0;
+	x = 0;
+	while(argv[x + 1] != '\0')
+	{
+		if (argv[x] != ' ')
+		{
+			if (argv[x + 1] == '\0')
+				num++;
+		}
+		else if (argv[x] == ' ')
+		{
+			if ((x != 0) && (argv[x - 1] != ' '))
+				num++;
+		}
+		x++;
+	}
+	return (num);
+}
+
+static int	ft_len(char *s)
+{
+	int	x;
+
+	x = 0;
+	while (s[x] != '\0' && s[x] != ' ')
+		x++;
+	return (x);
+}
+
+int get_word(char *s, t_strc_gen *est)
+{
+	int		x;
+	int		wlen;
+	char	*aux;
+
+	x = 0;
+	wlen = ft_len(s);
+	aux = malloc(sizeof(char) * (wlen + 1));
+	if (!aux)
+		return (0);
+	while (x < wlen)
+	{
+		aux[x] = s[x];
+		x++;
+	}
+	aux[wlen] = '\0';
+	x = ft_atoi(aux, est);
+	return (x);
+}
+
+int *ft_split(char *s, int *a, int y, t_strc_gen *est)
+{
+	int *aux;
+	int	x;
+	int	countw;
+
+	x = 0;
+	if (!s)
+		return (NULL);
+	countw = ft_count_arg(s);
+	aux = malloc(sizeof(int) * (countw + y));
+	while (x != y)
+	{
+		aux[x] = a[x];
+		x++;
+	}
+	if (!aux)
+		return (NULL);
+	while (countw > 0)
+	{
+		while (*s == ' ' && *s != '\0')
+			s++;
+		aux[x] = get_word(s, est);
+		while (*s != ' ' && *s != '\0')
+			s++;
+		x++;
+		countw--;
+	}
+	return (aux);
+}
+
+// ----------------------------------split
+int *ft_get_number(int argc, char **argv, t_strc_gen *est)
+{
+	/*
+		aqui recojo todos los numeros para tenerlos como int y poder trabajar con ellos 
+		Lo primero es tratar los numeros(argumentos) uno a uno y pasarlos por el split y por el atoi
+		El split es para quitar los argumentos pasados como "", de tal manera que los tomo y los paso a numeros separados
+		El atoi no solo me pasa los argumentos a int si no que tambien lo utilizo para comprobar que los nueros sean numeros
+
+	*/
 	int	*a;
 	int aux;
 	int	x;
+	int num;
 
 	aux = 0;
 	x = 0;
-	a = malloc(sizeof(int) * len);
-	while (x != len)
+	a = malloc(sizeof(int) * argc);
+	//recorro todos los argumentos
+	while (x != argc)
 	{
-		aux = ft_atoi(argv[x + 1], est);
+		num = ft_count_arg(argv[x + 1]);
+		//printf("\n num : %i\n", num);
+		if(num < 0)
+			est->error = 1;
+		else if(num == 0)
+		{
+			aux = ft_atoi(argv[x + 1], est);
+			
+			a[x] = aux;
+			printf(" <%i> ", a[x]);
+		}
+		else
+		{
+			a = ft_split(argv[x + 1], a, x, est);
+			printf(" <%i> ", a[x]);
+		}
 		if (est->error != 0)
 			return (0);
-		a[x] = aux;
-		printf(" <%i> ", a[x]);
+		
 		x++;
 	}
 	return (a);
@@ -69,7 +179,6 @@ struct s_strc ft_make_c(t_strc_gen est)
 	
 	x = 0;
 	y = 1;
-	printf("\n1 llego\n");
 	est.c = malloc(sizeof(int) * est.longa);
 	while (x != est.longa)
 	{
@@ -98,16 +207,8 @@ struct s_strc ft_make_c(t_strc_gen est)
 			x++;
 		}
 	}
-	printf("\n2 llego\n");
 	return (est);
 }
-
-/*void ft_clean_max(t_strc_gen est)
-{
-	//free(est.a);
-	//free(est.b);
-	//free(est.c);
-}*/
 
 int	main(int argc, char **argv)
 {
@@ -122,42 +223,24 @@ int	main(int argc, char **argv)
 	est.count_mov = 0;
 	est.a = ft_get_number(est.longa, argv, &est);
 	est.longc = est.longa;
-	printf("\nllego\n");
 	if (est.error != 0)
 	{
-		printf("ERROR\n");
+		write(1, "Error 1\n", 7);
 		return (0);
 	}
 	est = ft_make_c(est);
 	est.mid = est.c[est.longa/2];
-	printf("\n3 llego\n");
-	printf("    .%i.  ", est.mid);
 	if (est.error != 0)
 	{
-		printf("ERROR\n");
+		write(1, "Error 2\n", 7);
 		return (0);
 	}
-	printf("\n4 llego\n");
 	if (est.longa <= 3)
-	{
-		printf("\n3 o menos\n");
 		est = ft_three(est);
-	}
-		
 	else if (est.longa <= 100)
-	{
-		printf("\n100 o menos\n");
 		est = ft_one_hun(est);
-	}
-		
 	else
-	{
-		printf("ERROR\n");
 		est = ft_five_hun(est);
-		
-	}
-		
-		
 	x = 0;
 	while (x != est.longa)
 	{
